5d8eaf942fa4966b3408abfc1a00967562e3760a
K1sek1 <166694628+K1sek1@users.noreply.github.com>
2024-04-12 14:44:49 +0900

    Delete sample-site directory

--- sample-site/scripts/main.js
+++
@@ -1,5 +0,0 @@
-for (const elm of document.querySelectorAll(".dummy"))
-  elm.onfocus = () => elm.blur();
-
-for (const elm of document.querySelectorAll(".body *"))
-  elm.onfocus = () => elm.select();
